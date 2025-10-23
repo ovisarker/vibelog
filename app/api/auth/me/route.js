8446b2962e4268d48c2016c1d208c077fcf03e1c
Ovi Sarker <144815026+ovisarker@users.noreply.github.com>
2025-10-23 09:48:36 +0600

    Update route.js

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,4 +1,10 @@
-import { NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect";
-import User from "@/models/User";
-import jwt from "jsonwebtoken";
+export async function GET() {
+  try {
+    const res = await fetch("https://68f9a434ef8b2e621e7cf4fa.mockapi.io/users");
+    const users = await res.json();
+
+    return new Response(JSON.stringify(users), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ message: err.message }), { status: 500 });
+  }
+}
