4027c2b5f9c96732ca780a33479fd046152eb351
Ovi Sarker <144815026+ovisarker@users.noreply.github.com>
2025-10-23 10:45:27 +0600

    Update layout.js

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,10 +1,13 @@
+// app/dashboard/layout.js
 import Sidebar from "@/components/Sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="flex min-h-screen bg-gray-950 text-gray-100">
+    <div className="flex">
       <Sidebar />
-      <main className="flex-1 p-6">{children}</main>
+      <main className="flex-1 ml-[60px] md:ml-[220px] min-h-screen bg-gray-950 text-white p-8 transition-all duration-500">
+        {children}
+      </main>
     </div>
   );
 }
