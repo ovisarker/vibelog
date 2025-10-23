b97da3a6497b99d0036b273698123b47ff05e9e4
Ovi Sarker <144815026+ovisarker@users.noreply.github.com>
2025-10-23 10:30:19 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+// app/page.js
+export default function HomePage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen text-gray-100 bg-gray-950">
+      <h1 className="text-4xl font-bold mb-4">Welcome to VibeLog 💫</h1>
+      <p className="text-gray-400">
+        Track your moods and visualize your emotional journey.
+      </p>
+    </main>
+  );
+}
